cbcc076c6bcbb24b1b27038bfa5500b495a457da
minyee <mt3126@columbia.edu>
2020-05-24 23:24:30 -0400

    finished implementing critmat. Currently uses KMeans to first get the desired number of clusters, and then finding the critical matrix in each cluster.

--- critmat.py
+++ critmat.py
@@ -1,5 +1,6 @@
 import os, sys, copy, math
 import numpy as np
+from sklearn.cluster import KMeans
 
 ## The algorithm for computing the representative vectors given a collection
 ## of vectors. The 
@@ -8,13 +9,33 @@ class CritMat(object):
 		self.number_of_training_snapshots = len(training_vectors)
 		self.training_traffic_vectors =  training_vectors
 		return
+	# checks if v1 dominates v2
+	def __dominated(self, v1, v2):
+		dims = len(v1)
+		for i in range(dims):
+			if v2[i] > v1[i]:
+				return False
+		return True
 
-	## returns the cost(i,j), essentially the cost function
-	def _cost(self, vector_i, vector_j):
-		distance = 0
-		for index in range(len(vector_i)):
-			distance += (vector_i[index] - vector_j[index]) ** 2
-		return math.sqrt(distance)
+	def __trim(self):
+		dominated = [False] * self.number_of_training_snapshots
+		for i in range(self.number_of_training_snapshots - 1):
+			is_dominated = dominated[i]
+			if is_dominated:
+				continue
+			else:
+				for j in range(self.number_of_training_snapshots):
+					if j != i and not dominated[j] and self.__dominated(self.training_traffic_vectors[j], self.training_traffic_vectors[i]):
+						dominated[i] = True
+						is_dominated = True
+						break
+		remaining_entries = 0
+		for boolean in dominated:
+			if not boolean:
+				remaining_entries += 1
+		print("trimmed, remaining entries : {}".format(remaining_entries))
+		return
+
 
 	# Computes the euclidean distance between two vectors. 
 	# A helper function for other routines.
@@ -24,83 +45,122 @@ class CritMat(object):
 			distance += (vector_i[i] - vector_j[i]) ** 2
 		return math.sqrt(distance)
 
-	def _cost_euclidean(self, vector_dimensions, vector_i, vector_j):
-		head_ij = self._find_head(vector_dimensions, [vector_i, vector_j])
+	def _cost_euclidean(self, vector_dimensions, head_ij, vector_i, vector_j):
 		return max(self.__euclidean_distance(vector_dimensions, head_ij, vector_i), self.__euclidean_distance(vector_dimensions, head_ij, vector_j))
 
-	## Given a collection of vectors, finds the head vector, which is a vector that dominates 
-	## all other vectors in every dimension of the vector
-	def _find_head(self, vector_dimension, collection_of_vectors):
-		head = np.zeros((vector_dimension,))
-		for i in range(vector_dimension):
-			for vector in collection_of_vectors:
-				head[i] = max(head[i], vector[i])
-		return head
-
-	def check_valid(self):
-		return
-
-	## Derives the critical representative traffic matrices
-	def train(self, number_of_clusters=1):
-		number_of_clusters = int(number_of_clusters)
-		assert(number_of_clusters >= 1)
-		assert(self.number_of_training_snapshots >= number_of_clusters)
-		vector_dimensions = len(self.training_traffic_vectors[0])
-
+	## Critical-ness Aware Clustering
+	def __CritAC__(self, training_vectors, number_of_clusters):
+		vector_dimensions = len(training_vectors[0])
+		num_training_snapshots = len(training_vectors)
+		
 		## Step 1 : Initialization phase
 		cluster_heads = []
 		clusters = []
-		intercluster_scores = [0] * (self.number_of_training_snapshots * (self.number_of_training_snapshots - 1) / 2)
-		for cluster_id in range(self.number_of_training_snapshots):
-			cluster_heads.append(copy.deepcopy(self.training_traffic_vectors[cluster_id]))
-			clusters.append(copy.deepcopy(self.training_traffic_vectors[cluster_id]))
-		print("weee")
-		## compute the scores
-		print("num iterations: {}".format(self.number_of_training_snapshots * (self.number_of_training_snapshots - 1) / 2))
+		intercluster_scores = [0] * (num_training_snapshots * (num_training_snapshots - 1) / 2)
+		for cluster_id in range(num_training_snapshots):
+			cluster_heads.append(copy.deepcopy(training_vectors[cluster_id]))
+			clusters.append(copy.deepcopy(training_vectors[cluster_id]))
 		index = 0
-		for i in range(self.number_of_training_snapshots - 1):
-			for j in range(i + 1, self.number_of_training_snapshots, 1):
-				head_ij = self._find_head(vector_dimensions, [self.training_traffic_vectors[i], self.training_traffic_vectors[j]])
-				cost_ij = self._cost_euclidean(vector_dimensions, clusters[i], clusters[j])
-				intercluster_scores[index] = (i, j, cost_ij,)
+		for i in range(num_training_snapshots - 1):
+			for j in range(i + 1, num_training_snapshots, 1):
+				head_ij = np.maximum(cluster_heads[i], cluster_heads[j])
+				cost_ij = sum(head_ij) - max(sum(clusters[i]), sum(clusters[j]))
+				#cost_ij = max(sum([(a - b)**2 for a, b in zip(head_ij, clusters[i])]), sum([(a - b)**2 for a, b in zip(head_ij, clusters[j])]))
+				intercluster_scores[index] = (i, j, cost_ij)
 				index += 1
 			print("{}".format(i))
 		intercluster_scores.sort(key=lambda x:x[2]) 
 		print("Initialization done")
+
 		## Step 2 : Agglomeration steps
 		## To remove a cluster index at j, we just move it into the tail, so that after each
 		## step, we reduce the leftover clusters by 1. This means that the clusters at the 
 		## tail are not considered anymore
-		percentage = 0.05
-		number_of_steps = self.number_of_training_snapshots - number_of_clusters
-		for step in range(self.number_of_training_snapshots - number_of_clusters):
-			leftover_clusters = self.number_of_training_snapshots - step
+		for step in range(num_training_snapshots - number_of_clusters):
+			leftover_clusters = num_training_snapshots - step
 			## agglomerate the two clusters that have the least cost
 			(i, j, score) = intercluster_scores[0]
-			new_head = self._find_head(vector_dimensions, [cluster_heads[i], cluster_heads[j], ])
-			cluster_heads[i] = new_head
-
+			#dist = [(a - b)**2 for a, b in zip(vector1, vector2)]
+			head_ij = np.maximum(cluster_heads[i], cluster_heads[j])
+			cluster_heads[i] = head_ij
+			index_to_merge = j
+			if sum(clusters[j] < clusters[i]):
+				index_to_merge = i
 			## Swap cluster j and head j to the last entry, which is the entry we no longer 
 			## look at in the subsequent interation here
 			tmp = clusters[leftover_clusters - 1]
-			clusters[leftover_clusters - 1] = clusters[j]
-			clusters[j] = tmp
+			clusters[leftover_clusters - 1] = clusters[index_to_merge]
+			clusters[index_to_merge] = tmp
 			tmp = cluster_heads[leftover_clusters - 1]
-			cluster_heads[leftover_clusters - 1] = cluster_heads[j]
-			cluster_heads[j] = tmp
-
+			cluster_heads[leftover_clusters - 1] = cluster_heads[index_to_merge]
+			cluster_heads[index_to_merge] = tmp
 
 			## remove the cluster that is not new_head
-			intercluster_scores = []
-			for ii in range(leftover_clusters - 2):
-				for jj in range(ii + 1, leftover_clusters - 1, 1):
-					head_ij = self._find_head(vector_dimensions, [self.training_traffic_vectors[ii], self.training_traffic_vectors[jj]])
-					cost_ij = self._cost_euclidean(vector_dimensions, clusters[ii], clusters[jj])
-					intercluster_scores.append((ii, jj, cost_ij,))
+			leftover_clusters -= 1
+			intercluster_scores = [(0,0,0)] * ((leftover_clusters) * (leftover_clusters - 1) / 2)
+			index = 0
+			for ii in range(leftover_clusters - 1):
+				for jj in range(ii + 1, leftover_clusters, 1):
+					head_ij = np.maximum(cluster_heads[ii], cluster_heads[jj])
+					cost_ij = sum(head_ij) - max(sum(clusters[i]), sum(clusters[j]))
+					#cost_ij = max(sum([(a - b)**2 for a, b in zip(head_ij, clusters[ii])]), sum([(a - b)**2 for a, b in zip(head_ij, clusters[jj])]))
+					intercluster_scores[index] = (i, j, cost_ij)
+					index += 1
 			# sorts the intercluster scores based on cost (sorts in place)
 			intercluster_scores.sort(key=lambda x:x[2]) 
 			print("step {}".format(step))
-			#if percentage <= float(step ) / number_of_steps:
-			#	print("training {}% COMPLETED...".format(float(step ) / number_of_steps))
-			#	percentage += 0.05
+		check_vector = np.zeros((vector_dimensions,))
+		for i in range(vector_dimensions):
+			check_vector[i] = cluster_heads[0][i] - training_vectors[0][i]
+		print("check vector \n\n{}\n\n".format(check_vector))
 		return [np.array(x) for x in cluster_heads[:number_of_clusters]]
+
+	def __kmc(self, training_vectors, number_of_clusters):
+		vector_dimensions = len(self.training_traffic_vectors[0])
+		## need to copy the vectors one by one
+		## use k means to first find the cluster centroids, find out the dominating cluster heads
+		# substep 1 : run k means clustering
+		kmeans = KMeans(n_clusters=number_of_clusters, random_state=0)
+		kmeans.fit(training_vectors)
+		# substep 2 : figure out all the vectors, and which cluster they are binned into
+		point_labels = kmeans.predict(training_vectors)
+		# substep 3 : for each cluster centroid, find the points that belong to this cluster, and find the head of each cluster
+		training_vectors = []
+		for _ in range(number_of_clusters):
+			head = np.zeros( (vector_dimensions,) )
+			training_vectors.append(head)
+		for training_vector, cluster_label in zip(self.training_traffic_vectors, point_labels):
+			training_vectors[cluster_label] = np.maximum(training_vector, training_vectors[cluster_label])
+		return training_vectors
+
+	## Derives the critical representative traffic matrices
+	def train(self, number_of_clusters=1, critac_snapshot_limit=100):
+		number_of_clusters = int(number_of_clusters)
+		assert(number_of_clusters >= 1)
+		assert(self.number_of_training_snapshots >= number_of_clusters)
+		assert(critac_snapshot_limit > 1)
+		vector_dimensions = len(self.training_traffic_vectors[0])
+		training_vectors = []
+
+		return self.__kmc(self.training_traffic_vectors, number_of_clusters)
+
+		if len(self.training_traffic_vectors) <= critac_snapshot_limit:
+			training_vectors = self.training_traffic_vectors
+		else:
+			## need to copy the vectors one by one
+			## use k means to first find the cluster centroids, find out the dominating cluster heads
+			# substep 1 : run k means clustering
+			kmeans = KMeans(n_clusters=critac_snapshot_limit, random_state=0)
+			kmeans.fit(self.training_traffic_vectors)
+			# substep 2 : figure out all the vectors, and which cluster they are binned into
+			point_labels = kmeans.predict(self.training_traffic_vectors)
+			# substep 3 : for each cluster centroid, find the points that belong to this cluster, and find the head of each cluster
+			training_vectors = []
+			for _ in range(critac_snapshot_limit):
+				head = np.zeros( (vector_dimensions,) )
+				training_vectors.append(head)
+			assert(len(point_labels) == len(self.training_traffic_vectors))
+
+			for training_vector, cluster_label in zip(self.training_traffic_vectors, point_labels):
+				training_vectors[cluster_label] = np.maximum(training_vector, training_vectors[cluster_label])
+		return self.__CritAC__(training_vectors, number_of_clusters)
--- test.py
+++ test.py
@@ -1,13 +1,31 @@
 import critmat
-import sys, os
+import sys, os, math
+import numpy as np
 sys.path.append("..")
 import facebook_dcn_traffic.traffic_snapshot_pb2
 import facebook_dcn_traffic.utility
 from sklearn.cluster import KMeans
 
-def read_in_facebook_traffic(trace_file_name, network_ids_filename):
+## import traffic engineering stuff
+import robust_topology_engineering.aurora_network as aurora_module
+import robust_topology_engineering.traffic_engineer as te_module
+import robust_topology_engineering.topology_engineer as toe_module
+import robust_topology_engineering.path_selector as path_selector_module
+
+## import for plotting uses 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+from matplotlib import cm
+
+def read_in_facebook_traffic(trace_file_name, network_ids_filename, aggregation_window):
 	valid_network_ids = facebook_dcn_traffic.utility.read_in_network_ids(network_ids_filename)
 	traffic_matrices = facebook_dcn_traffic.utility.read_traffic_matrix_protobuf(trace_file_name, considered_network_id=valid_network_ids)
+	print("Number of traffic matrices : {}".format(len(traffic_matrices)))
+	for tm in traffic_matrices:
+		for i in range(len(tm)):
+			for j in range(len(tm)):
+				tm[i][j] = tm[i][j] / 1000000. / float(aggregation_window)
 	return traffic_matrices, valid_network_ids
 
 def flatten_traffic_matrix(tm, num_nodes):
@@ -16,33 +34,269 @@ def flatten_traffic_matrix(tm, num_nodes):
 		for j in range(num_nodes):
 			if i != j:
 				vector.append(tm[i][j])
-	return vector
+	return np.array(vector)
 
-def main(trace_filename, network_ids_filename):
-	traffic_matrices, valid_network_ids = read_in_facebook_traffic(trace_filename, network_ids_filename)
+def _compute_link_utilization_statistics(nblocks, link_utilization_matrix, adj_matrix, total_links):
+		total_links = 0.
+		lu_distribution = []
+		total_weighted_lu = 0.
+		for i in range(nblocks):
+			for j in range(nblocks):
+				if i != j:
+					lu_distribution.append( (link_utilization_matrix[i][j], adj_matrix[i][j], ) )
+					total_links += adj_matrix[i][j]
+					total_weighted_lu += (adj_matrix[i][j] * link_utilization_matrix[i][j])
+		lu_distribution_sorted = sorted(lu_distribution, key=lambda x: x[0])	
+		cumulative_link_counts = (total_links - total_links)
+		p50_index = 0.5 * (total_links)
+		p90_index = 0.9 * (total_links)
+		lu50 = -1.
+		lu90 = -1.
+		mlu = lu_distribution_sorted[-1][0]
+		for (lu, num_links) in lu_distribution_sorted:
+			if lu50 < 0 and cumulative_link_counts >= p50_index:
+				lu50 = lu
+			elif lu90 < 0 and cumulative_link_counts >= p90_index:
+				lu90 = lu
+			cumulative_link_counts += num_links
+		alu = total_weighted_lu / total_links
+		return (mlu, lu90, lu50, alu, lu_distribution)
+
+# computes the routing performance for a topology with a given set of routing weights
+# part of static routing
+def _evaluate_snapshot_performance(traffic_matrix, topology_adj_matrix, routing_weights, link_capacity, total_links, return_lu_distribution=False):
+	nblocks = len(topology_adj_matrix)
+	link_utilization = np.zeros((nblocks, nblocks,))
+	hop_counter = 0.
+	total_traffic = 0.
+	#unable_to_route = False
+	for path in routing_weights:
+		src = path[0]
+		dst = path[-1]
+		weight = routing_weights[path]
+		if traffic_matrix[src][dst] == 0.:
+			continue
+		if weight < 0.00001:
+			continue
+		traffic_load = weight * traffic_matrix[src][dst]
+		
+		total_traffic += traffic_load
+		hop_counter += ((len(path) - 1) * traffic_load)
+		curr_node = src
+		for index in range(1, len(path), 1):
+			next_node = path[index]
+			path_cap = link_capacity * topology_adj_matrix[curr_node][next_node]
+			#if path_cap == 0. and traffic_load >= 0.0001:
+				#assert(false)
+			if traffic_load / path_cap > 2.:
+				print("link_load exploded! from src: {} to dst: {} via path {}".format(src, dst, path))
+				print("traffic demand: {} weight : {}, adj_matrix has number of links : {}".format(traffic_matrix[src][dst], weight, topology_adj_matrix[curr_node][next_node]))
+
+			link_utilization[curr_node][next_node] += (traffic_load / path_cap)
+			curr_node = next_node
+	ave_hop_count = hop_counter / total_traffic
+	(mlu, lu90, lu50, alu, lu_distribution) = _compute_link_utilization_statistics(nblocks, link_utilization, topology_adj_matrix, total_links)
+	if not return_lu_distribution:
+		return (mlu, lu90, lu50, ave_hop_count)
+	else:
+		return (mlu, lu90, lu50, ave_hop_count, [x[0] for x in lu_distribution])
+
+def main(trace_filename, network_ids_filename, aggregation_window):
+	traffic_matrices, valid_network_ids = read_in_facebook_traffic(trace_filename, network_ids_filename, aggregation_window)
+	traffic_matrices = traffic_matrices[1:-1]
+
+	considered_snapshots = 3000
+	offset = 3000
+	traffic_matrices = traffic_matrices[offset:offset + considered_snapshots]
+
 	number_of_pods = len(valid_network_ids)
 	assert(number_of_pods == len(traffic_matrices[0]))
 	print("Finished reading the traffic matrices")
 	traffice_vectors = [flatten_traffic_matrix(x, number_of_pods) for x in traffic_matrices]
 
-	# run kmc
-	kmeans = KMeans(n_clusters=3, random_state=0).fit(traffice_vectors)
-	cluster_centroids = kmeans.cluster_centers_
+	number_of_snapshots = len(traffice_vectors)
+	#for i in range(number_of_snapshots * (number_of_snapshots - 1) / 2):
+	#	print("{}".format(i))
+	#for i in range(number_of_snapshots - 1):
+	#	for j in range(i + 1, number_of_snapshots, 1):
+	#		print("{}, {}".format(i, j))
 
-	print("flatened vectors have dimension = {}".format(len(traffice_vectors[0])))
+	#for i in range(len(traffice_vectors) - 1):
+	#	for j in range(i+1, len(traffice_vectors), 1):
+	#		if check_dominate(traffice_vectors[i], traffice_vectors[j]):
+	#			print("{} dominates {}".format(i, j))
+
 	print("Finished flattening the traffic matrices into vectors")
+
+	num_clusters = 4
+
 	critm = critmat.CritMat(traffice_vectors, None)
 	print("total number of traffic matrices : {}".format(len(traffice_vectors)))
-	representative_vectors = critm.train(number_of_clusters=3)
-	print("\n\n\n")
-	for index, representative_vector in zip(range(len(representative_vectors)), representative_vectors):
-		print("{}. {}".format(index, representative_vector))
+	representative_vectors = critm.train(number_of_clusters=num_clusters)
+
+	maximum_vector = np.zeros((number_of_pods * (number_of_pods - 1),))
+	for traffic_vector in traffice_vectors:
+		maximum_vector = np.maximum(maximum_vector, traffic_vector)
+
+
+	#if num_clusters == 1:
+	#	assert(len(representative_vectors) == 1)
+	#	lefover = np.zeros((len(maximum_vector),))
+	#	for i in range(len(lefover)):
+	#		lefover[i] = maximum_vector[i] - representative_vectors[0][i]
+	#	print("max vector : {}".format(maximum_vector))
+	#	print("representative : {}".format(representative_vectors[0]))
+	#	print("leftover array : \n{}".format(lefover))
+	#	exit()
+
+	representative_matrices = []
+	nrow = int(math.ceil(math.sqrt(num_clusters)))
+	ncol = int(math.ceil(float(num_clusters) / nrow))
+	fig, axes = plt.subplots(nrows=nrow, ncols=ncol, squeeze=True)
+	axes_vector = [axes]
+	if num_clusters > 1:
+		axes_vector = axes.flatten()
+	for ax, index in zip(axes_vector, range(num_clusters)):
+		traffic_matrix = np.zeros((number_of_pods, number_of_pods,))
+		offset = 0
+		for i in range(number_of_pods):
+			for j in range(number_of_pods):
+				if i != j:
+					traffic_matrix[i][j] = representative_vectors[index][offset]
+					offset += 1
+		ax.imshow(traffic_matrix)
+		representative_matrices.append(traffic_matrix)
+	#plt.title("{}".format(cluster_alias), fontsize=14)
+	
+
+	# Define the traffic_engineering class
+	# Uniform Topology
+	uniform_topology = [0] * number_of_pods
+
+	per_node_pair_num_links = 10
+	for i in range(number_of_pods):
+		uniform_topology[i] = [0] * number_of_pods
+		for j in range(number_of_pods):
+			if i != j:
+				uniform_topology[i][j] = per_node_pair_num_links
+
+	link_capacity = 5
+	block_params = {aurora_module.BlockType.SUPERBLOCK : {}, aurora_module.BlockType.BORDER_ROUTER : {}}
+	block_params[aurora_module.BlockType.SUPERBLOCK]["link capacity"] = float(link_capacity)
+	block_params[aurora_module.BlockType.SUPERBLOCK]["num links"] = float((number_of_pods - 1) * per_node_pair_num_links)
+	block_params[aurora_module.BlockType.BORDER_ROUTER]["link capacity"] = float(link_capacity) # in gbps
+	block_params[aurora_module.BlockType.BORDER_ROUTER]["num links"] = float((number_of_pods - 1) * per_node_pair_num_links)
+	block_names_list = range(1, number_of_pods + 1, 1)
+	block_names_list = ["ju{}".format(x) for x in block_names_list]
+	aurora_network = aurora_module.AuroraNetwork("customized", block_params, block_names_list)
+	allpaths_selector = path_selector_module.PathSelector(aurora_network, use_multihop=True)
+	all_interblock_paths = allpaths_selector.get_all_paths()
+	directpaths_selector = path_selector_module.PathSelector(aurora_network, use_multihop=False)
+	direct_interblock_paths = directpaths_selector.get_all_paths()
+
+
+
+	## Topology Engineer classes
+	max_toe = toe_module.HistoricalMaxTrafficTopologyEngineer(aurora_network, 10, 10, all_interblock_paths, traffic_matrices)
+	max_topology = max_toe.topology_engineer_given_TMs(traffic_matrices, all_interblock_paths)
+	max_topology = aurora_network.round_fractional_topology_giant_switch(max_topology, [])
+	multi_tm_toe = toe_module.RobustMultiTrafficTopologyEngineer(aurora_network, 10,
+																10, all_interblock_paths, 
+																traffic_matrices, num_clusters)
+	multi_tm_topology = multi_tm_toe.topology_engineer_given_representative_TMs(representative_matrices, all_interblock_paths)
+	multi_tm_topology = aurora_network.round_fractional_topology_giant_switch(multi_tm_topology, [])
+	ave_toe = toe_module.HistoricalMaxTrafficTopologyEngineer(aurora_network, 10, 10, all_interblock_paths, traffic_matrices)
+	ave_topology = ave_toe.topology_engineer_given_TMs(traffic_matrices, all_interblock_paths)
+	ave_topology = aurora_network.round_fractional_topology_giant_switch(ave_topology, [])
+
+	## max tm
+	max_tm = np.zeros((number_of_pods, number_of_pods))
+	for tm in traffic_matrices:
+		for i in range(number_of_pods):
+			for j in range(number_of_pods):
+				max_tm[i][j] = max(max_tm[i][j], tm[i][j])
+
+	## Compute the routing weights - Uniform topology
+	multi_tm_traffic_engineer = te_module.RobustMultiClusterScaleUpWeightedTrafficEngineer(aurora_network, all_interblock_paths, 4, 1, num_clusters, reduce_multihop=True)
+	unif_multi_tm_routing_weights = multi_tm_traffic_engineer.compute_path_weights(uniform_topology, representative_matrices)
+	#multi_tm_routing_weights = multi_tm_traffic_engineer.compute_path_weights(uniform_topology, [max_tm,])
+	
+	max_traffic_engineering = te_module.HistoricalMaxTrafficEngineer(aurora_network, all_interblock_paths, 10, 10)
+	unif_max_routing_weights = max_traffic_engineering.compute_path_weights(uniform_topology, traffic_matrices)
+
+	ave_traffic_engineering = te_module.HistoricalAveTrafficEngineer(aurora_network, all_interblock_paths, 10, 10)
+	unif_ave_routing_weights = ave_traffic_engineering.compute_path_weights(uniform_topology, traffic_matrices)
+
+	## Compute the routing weights - Topology Engineer
+	multi_toe_multi_te_routing_weights = multi_tm_traffic_engineer.compute_path_weights(multi_tm_topology, representative_matrices)
+	#multi_tm_routing_weights = multi_tm_traffic_engineer.compute_path_weights(uniform_topology, [max_tm,])
+	
+	max_toe_max_te_routing_weights = max_traffic_engineering.compute_path_weights(max_topology, traffic_matrices)
+
+	ave_toe_ave_te_routing_weights = ave_traffic_engineering.compute_path_weights(ave_topology, traffic_matrices)
+
+	unif_multi_tm_mlu = []
+	unif_max_tm_mlu = []
+	unif_ave_tm_mlu = []
+	unif_multi_tm_ahc = []
+	unif_max_tm_ahc = []
+	unif_ave_tm_ahc = []
+
+	multi_toe_multi_te_mlu = []
+	ave_toe_ave_te_mlu = []
+	max_toe_max_te_mlu = []
+	multi_toe_multi_te_ahc = []
+	ave_toe_ave_te_ahc = []
+	max_toe_max_te_ahc = []
+	total_links = sum([sum(x) for x in uniform_topology])
+	for tm in traffic_matrices:
+		unif_multi_tm_perf = _evaluate_snapshot_performance(tm, uniform_topology, unif_multi_tm_routing_weights, link_capacity, total_links, return_lu_distribution=False)
+		unif_max_tm_perf = _evaluate_snapshot_performance(tm, uniform_topology, unif_max_routing_weights, link_capacity, total_links, return_lu_distribution=False)
+		unif_ave_tm_perf = _evaluate_snapshot_performance(tm, uniform_topology, unif_ave_routing_weights, link_capacity, total_links, return_lu_distribution=False)
+		unif_multi_tm_mlu.append(unif_multi_tm_perf[0])
+		unif_max_tm_mlu.append(unif_max_tm_perf[0])
+		unif_ave_tm_mlu.append(unif_ave_tm_perf[0])
+		unif_multi_tm_ahc.append(unif_multi_tm_perf[3])
+		unif_max_tm_ahc.append(unif_max_tm_perf[3])
+		unif_ave_tm_ahc.append(unif_ave_tm_perf[3])
+
+		multi_toe_multi_te_perf = _evaluate_snapshot_performance(tm, multi_tm_topology, multi_toe_multi_te_routing_weights, link_capacity, total_links, return_lu_distribution=False)
+		ave_toe_ave_te_perf = _evaluate_snapshot_performance(tm, ave_topology, ave_toe_ave_te_routing_weights, link_capacity, total_links, return_lu_distribution=False)
+		max_toe_max_te_perf = _evaluate_snapshot_performance(tm, max_topology, max_toe_max_te_routing_weights, link_capacity, total_links, return_lu_distribution=False)
+		multi_toe_multi_te_mlu.append(multi_toe_multi_te_perf[0])
+		ave_toe_ave_te_mlu.append(ave_toe_ave_te_perf[0])
+		max_toe_max_te_mlu.append(max_toe_max_te_perf[0])
+		multi_toe_multi_te_ahc.append(multi_toe_multi_te_perf[3])
+		ave_toe_ave_te_ahc.append(ave_toe_ave_te_perf[3])
+		max_toe_max_te_ahc.append(max_toe_max_te_perf[3])
+	fig = plt.figure()
+	plt.plot(sorted(unif_multi_tm_mlu))
+	plt.plot(sorted(unif_max_tm_mlu))
+	plt.plot(sorted(unif_ave_tm_mlu))
+	plt.plot(sorted(multi_toe_multi_te_mlu))
+	plt.plot(sorted(max_toe_max_te_mlu))
+	plt.plot(sorted(ave_toe_ave_te_mlu))
+	plt.legend(["unif - multi tm", "unif - max", "unif - ave", "multi toe - multi tm", "max toe - max te", "ave toe - ave te"])
+	plt.xlim(xmin=0)
+	plt.ylim(ymin=0)
+
+	fig = plt.figure()
+	plt.plot(sorted(unif_multi_tm_ahc))
+	plt.plot(sorted(unif_max_tm_ahc))
+	plt.plot(sorted(unif_ave_tm_ahc))
+	plt.plot(sorted(multi_toe_multi_te_ahc))
+	plt.plot(sorted(max_toe_max_te_ahc))
+	plt.plot(sorted(ave_toe_ave_te_ahc))
+	plt.legend(["unif - multi tm", "unif - max", "unif - ave", "multi toe - multi tm", "max toe - max te", "ave toe - ave te"])
+	plt.xlim(xmin=0)
+	plt.ylim(ymin=1, ymax=2)
+	plt.show()
 	return
 
 
 if __name__ == "__main__":
 	print("Critmat test")
-
+	'''
 	test_vectors = [[3, 3, 1],
 					[0, 2, 4], 
 					[4, 2, 0],
@@ -65,10 +319,11 @@ if __name__ == "__main__":
 
 	for traffic_matrix, index in zip(representative_traffic_matrices, range(1, len(representative_traffic_matrices) + 1, 1)):
 		print("{}. {}".format(index, traffic_matrix) )
-
-	tm_snapshots_protobuf_filename = "/Users/minyee/src/facebook_dcn_traffic/traffic_matrices/clusterC/hadoop_aggregationwindow_30.pb"
-	valid_network_ids_filename = "/Users/minyee/src/facebook_dcn_traffic/traffic_matrices/clusterC/clusterC_pods.txt"
-	#tm_snapshots_protobuf_filename = "/Users/minyee/src/facebook_dcn_traffic/traffic_matrices/clustercombined/combined_aggregationwindow_1.pb"
-	#valid_network_ids_filename = "/Users/minyee/src/facebook_dcn_traffic/traffic_matrices/clustercombined/clustercombined_pods.txt"
-	main(tm_snapshots_protobuf_filename, valid_network_ids_filename)
+	'''
+	aggregation_window = 1
+	tm_snapshots_protobuf_filename = "/Users/minyee/src/facebook_dcn_traffic/traffic_matrices/clusterB/web_aggregationwindow_{}.pb".format(aggregation_window)
+	valid_network_ids_filename = "/Users/minyee/src/facebook_dcn_traffic/traffic_matrices/clusterB/clusterB_pods.txt"
+	tm_snapshots_protobuf_filename = "/Users/minyee/src/facebook_dcn_traffic/traffic_matrices/clustercombined/combined_aggregationwindow_1.pb"
+	valid_network_ids_filename = "/Users/minyee/src/facebook_dcn_traffic/traffic_matrices/clustercombined/clustercombined_pods.txt"
+	main(tm_snapshots_protobuf_filename, valid_network_ids_filename, aggregation_window)
 
